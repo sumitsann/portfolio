bd5341da7e9f53904aaa7c0e6a00ad7c4d5dbb3e
Sumit Ojha <sumitojha501@gmail.com>
2022-10-13 19:20:10 -0500

    Updates

---
+++ precache-manifest.056ee469e657427f6e422937f3b81c2c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc8f8da26ea26af07c2",
+    "url": "/portfolio/static/css/main.02f230d6.chunk.css"
+  },
+  {
+    "revision": "5cc8f8da26ea26af07c2",
+    "url": "/portfolio/static/js/main.5cc8f8da.chunk.js"
+  },
+  {
+    "revision": "29a72c534fa048826564",
+    "url": "/portfolio/static/js/1.29a72c53.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "17e5157018327a3df914d75fd895cb6d",
+    "url": "/portfolio/static/media/me.17e51570.png"
+  },
+  {
+    "revision": "4ba9b0fddc07d3e911446ad6c8fc4b0f",
+    "url": "/portfolio/static/media/award.4ba9b0fd.jpg"
+  },
+  {
+    "revision": "225b7d832fd1b6024d3982cda518e9e8",
+    "url": "/portfolio/static/media/about.225b7d83.jpg"
+  },
+  {
+    "revision": "3510a90df10dd57d90eeaf1a5ddf1b83",
+    "url": "/portfolio/static/media/sun.3510a90d.jpg"
+  },
+  {
+    "revision": "3f6c87447985a86a647ceef243710957",
+    "url": "/portfolio/static/media/moon.3f6c8744.jpg"
+  },
+  {
+    "revision": "924a623d3e906b6362739106f0f9657f",
+    "url": "/portfolio/static/media/phone.924a623d.jpg"
+  },
+  {
+    "revision": "6da6a3b82d81167e7aef9276bbea2c17",
+    "url": "/portfolio/static/media/email.6da6a3b8.jpg"
+  },
+  {
+    "revision": "7c4bb778f195658e88e51c13346adb19",
+    "url": "/portfolio/static/media/address.7c4bb778.png"
+  },
+  {
+    "revision": "8dad1581bcb5b8af5cf96f76bb76b0ea",
+    "url": "/portfolio/index.html"
+  }
+];
