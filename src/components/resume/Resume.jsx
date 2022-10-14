200062264326cb801dcb281800b30e5d0a09ca57
Sumit Ojha <sumitojha501@gmail.com>
2022-10-13 19:42:22 -0500

    Created resume module

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./resume.css";
+
+const Resume = () => {
+  return (
+    <div>
+      <div>
+        <h3>
+          Download Resume:{" "}
+          <a href="https://github.com/priyaravi23/react-portfolio/raw/master/src/assets/priya-resume.pdf">
+            Sumit Ojha
+          </a>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
